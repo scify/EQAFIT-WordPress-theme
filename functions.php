595a32c31fc0708af0190951142313dc3892f43f
Paul Isaris <paulisaris@gmail.com>
2022-09-06 13:10:27 +0300

    Reverted CORS configuration

--- functions.php
+++ functions.php
@@ -9,112 +9,111 @@
  */
 
 if (!defined('_S_VERSION')) {
-	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '1.0.12');
+    // Replace the version number of the theme on each release.
+    define('_S_VERSION', '1.0.12');
 }
 
 if (!function_exists('eqafit_setup')) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function eqafit_setup()
-	{
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on eqafit, use a find and replace
-		 * to change 'eqafit' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain('eqafit', get_template_directory() . '/languages');
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function eqafit_setup() {
+        /*
+         * Make theme available for translation.
+         * Translations can be filed in the /languages/ directory.
+         * If you're building a theme based on eqafit, use a find and replace
+         * to change 'eqafit' to the name of your theme in all the template files.
+         */
+        load_theme_textdomain('eqafit', get_template_directory() . '/languages');
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support('automatic-feed-links');
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support('automatic-feed-links');
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support('title-tag');
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support('title-tag');
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support('post-thumbnails');
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support('post-thumbnails');
 
-		require_once "wp_nav_menu_walker_bootstrap5.php";
+        require_once "wp_nav_menu_walker_bootstrap5.php";
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus(
-			array(
-				'menu-header' => esc_html__('Header Menu'),
-			)
-		);
+        // This theme uses wp_nav_menu() in one location.
+        register_nav_menus(
+            array(
+                'menu-header' => esc_html__('Header Menu'),
+            )
+        );
 
-		// Allow the menu to have a css class on the li elements
-		function add_additional_class_on_li($classes, $item, $args)
-		{
-			if (isset($args->add_li_class)) {
-				$classes[] = $args->add_li_class;
-			}
-			return $classes;
-		}
-		add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
+        // Allow the menu to have a css class on the li elements
+        function add_additional_class_on_li($classes, $item, $args) {
+            if (isset($args->add_li_class)) {
+                $classes[] = $args->add_li_class;
+            }
+            return $classes;
+        }
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support(
-			'html5',
-			array(
-				'search-form',
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-				'style',
-				'script',
-			)
-		);
+        add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'eqafit_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support(
+            'html5',
+            array(
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'style',
+                'script',
+            )
+        );
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support('customize-selective-refresh-widgets');
+        // Set up the WordPress core custom background feature.
+        add_theme_support(
+            'custom-background',
+            apply_filters(
+                'eqafit_custom_background_args',
+                array(
+                    'default-color' => 'ffffff',
+                    'default-image' => '',
+                )
+            )
+        );
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 250,
-				'width'       => 250,
-				'flex-width'  => true,
-				'flex-height' => true,
-			)
-		);
-	}
+        // Add theme support for selective refresh for widgets.
+        add_theme_support('customize-selective-refresh-widgets');
+
+        /**
+         * Add support for core custom logo.
+         *
+         * @link https://codex.wordpress.org/Theme_Logo
+         */
+        add_theme_support(
+            'custom-logo',
+            array(
+                'height' => 250,
+                'width' => 250,
+                'flex-width' => true,
+                'flex-height' => true,
+            )
+        );
+    }
 endif;
 add_action('after_setup_theme', 'eqafit_setup');
 
@@ -125,10 +124,10 @@ add_action('after_setup_theme', 'eqafit_setup');
  *
  * @global int $content_width
  */
-function eqafit_content_width()
-{
-	$GLOBALS['content_width'] = apply_filters('eqafit_content_width', 640);
+function eqafit_content_width() {
+    $GLOBALS['content_width'] = apply_filters('eqafit_content_width', 640);
 }
+
 add_action('after_setup_theme', 'eqafit_content_width', 0);
 
 /**
@@ -136,30 +135,30 @@ add_action('after_setup_theme', 'eqafit_content_width', 0);
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function eqafit_widgets_init()
-{
-	register_sidebar(
-		array(
-			'name'          => esc_html__('Sidebar', 'eqafit'),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__('Add widgets here.', 'eqafit'),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+function eqafit_widgets_init() {
+    register_sidebar(
+        array(
+            'name' => esc_html__('Sidebar', 'eqafit'),
+            'id' => 'sidebar-1',
+            'description' => esc_html__('Add widgets here.', 'eqafit'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget' => '</section>',
+            'before_title' => '<h2 class="widget-title">',
+            'after_title' => '</h2>',
+        )
+    );
 
-	register_sidebar(array(
-		'name' => 'Footer Sidebar 1',
-		'id' => 'footer-sidebar-1',
-		'description' => 'Appears in the footer area',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '</aside>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>',
-	));
+    register_sidebar(array(
+        'name' => 'Footer Sidebar 1',
+        'id' => 'footer-sidebar-1',
+        'description' => 'Appears in the footer area',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ));
 }
+
 add_action('widgets_init', 'eqafit_widgets_init');
 
 require_once 'functions/functions_add_scripts_and_styles.php';
@@ -189,10 +188,5 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 if (defined('JETPACK__VERSION')) {
-	require get_template_directory() . '/inc/jetpack.php';
+    require get_template_directory() . '/inc/jetpack.php';
 }
-
-function add_cors_http_header(){
-    header("Access-Control-Allow-Origin: *");
-}
-add_action('init','add_cors_http_header');
